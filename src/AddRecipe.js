28a0bc454715aeabd93e800429e6e9af4eb143c8
Scott Schwab <scottschwab86@gmail.com>
2023-07-01 14:54:10 -0500

    Get add recipe form running with fetch posting to db

--- src/AddRecipe.js
+++ src/AddRecipe.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function AddRecipe() {
 
+  const [newRecipeFormData, setNewRecipeFormData] = useState({
+    name: '',
+    image: '',
+    url: '',
+    notes: ''
+  })
+
+  console.log('FORM DATA STATE', newRecipeFormData);
+
+  function handleAddFormChange(e) {
+    setNewRecipeFormData({
+      ...newRecipeFormData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  function handleAddFormSubmit(e) {
+    e.preventDefault();
+    fetch('http://localhost:4000/recipes', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(newRecipeFormData)
+    })
+    .then(r => r.json())
+    .then(addedRecipe => console.log(addedRecipe))
+  }
+
   return (
     <div>
-      <h1>ADD NEW RECIPE FORM</h1>
+      <form onSubmit={handleAddFormSubmit}>
+        <input name="name" placeholder="Recipe Name" value={newRecipeFormData.name} onChange={handleAddFormChange}></input>
+        <input name="image" placeholder="Recipe Image URL" value={newRecipeFormData.image} onChange={handleAddFormChange}></input>
+        <input name="url" placeholder="Recipe Link URL" value={newRecipeFormData.url} onChange={handleAddFormChange}></input>
+        <input name="notes" placeholder="Notes" value={newRecipeFormData.notes} onChange={handleAddFormChange}></input>
+        <input type="submit"></input>
+      </form>
     </div>
   )
 }
