8b4ee2ca2d3103b04f0fa0d6270025244e222950
Scott Schwab <scottschwab86@gmail.com>
2023-07-01 12:24:56 -0500

    Add NavLinks to NavBar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
 
   return (
     <div>
-      <h3>NavBar</h3>
+      <h3>
+        <NavLink to='/'>Home</NavLink>
+      </h3>
+      <h3>
+        <NavLink to='/recipes'>My Recipes</NavLink>
+      </h3>
+      <h3>
+        <NavLink to='new'>Add New Recipe</NavLink>
+      </h3>
     </div>
   )
 }
