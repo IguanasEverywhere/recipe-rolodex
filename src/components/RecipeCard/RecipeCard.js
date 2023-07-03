efd7b37c64ba9c7047279dc46aff112d6da0f6be
Scott Schwab <scottschwab86@gmail.com>
2023-07-03 17:25:22 -0500

    Add Link to each RecipeCard

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import styles from './RecipeCard.module.css';
+import { Link, Route } from 'react-router-dom';
 
 function RecipeCard({ recipeInfo }) {
 
   const { name, image, url, notes } = recipeInfo;
 
   return (
-    <div className={styles['recipe-card']}>
-      <h3>{name}</h3>
-      <div className={styles['recipe-card-img']}>
-        <img src={image} alt={name}></img>
-      </div>
+    <Link exact to='/'>
+      <div className={styles['recipe-card']}>
+        <h3>{name}</h3>
+        <div className={styles['recipe-card-img']}>
+          <img src={image} alt={name}></img>
+        </div>
 
-      <a href={url}>{url}</a>
-      <p>Your Notes: {notes}</p>
-    </div>
+        <a href={url}>Link To Recipe</a>
+        <p>Your Notes: {notes}</p>
+      </div>
+    </Link>
   )
 
 
