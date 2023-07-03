4e491dc69af3d4eea183d8414dbd9f1e5da8bf9e
Scott Schwab <scottschwab86@gmail.com>
2023-07-03 16:43:17 -0500

    Clean up landing page with image

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import logo from '../../assets/images/cooker-king-AOVtEuU9UGc-unsplash.jpg'
+import styles from './Home.module.css'
 
 function Home() {
 
-  return(
-    <div>
-      <h1>Recipe Rolodex</h1>
+  return (
+    <div className={styles['landing-page']}>
+      <h1 className={styles.landingTitle}>Recipe Rolodex</h1>
+        <img src={logo} alt='rolodex-img' className={styles.landingImg}></img>
     </div>
   )
 }
 
 export default Home;
+
